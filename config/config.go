78bd824a614b91088f3aa93ec61ef5c66dc05f72
fer <klahnen@gmail.com>
2023-12-10 17:28:45 -0600

    absolute path for DB

--- config/config.go
+++ config/config.go
@@ -19,7 +19,7 @@ func GetConfig() Config {
 	config.Port, _ = getenvs.GetEnvInt("PORT", 8000)
 	config.ClientID = getenvs.GetEnvString("CLIENT_ID", "99001291aa1f4dacbf5b381f1fd8af71")
 	config.ClientSecret = getenvs.GetEnvString("CLIENT_SECRET", "4c7f1a7905d14e9bbc91116909a7bea1")
-	config.DBName = getenvs.GetEnvString("DB_NAME", "dev.db")
+	config.DBName = getenvs.GetEnvString("DB_NAME", "/tmp/dev.db")
 	config.RedirectURI = getenvs.GetEnvString("REDIRECT_URI", "http://127.0.0.1:8000/callback")
 	config.BearerToken = getenvs.GetEnvString("BEARER_TOKEN", "")
 
