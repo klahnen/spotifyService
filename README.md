a01b30335ae5095b52f39e005935cdf4ed7a7189
fer <klahnen@gmail.com>
2023-12-10 21:48:04 -0600

    feat: updated docs and removed postgres support

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # spotifyService
 
-## Getting the token
+## Getting started
 * start the server: `go run main.go` // Then follow the OAUTH2 process
 * stop the server `ctrl+c`
 * start the server again: `go run main.go`
+* once there the docs of the API are here: `http://127.0.0.1:8000/docs/index.html`
 
 ## Testing
 
-go test -v ./...
+`go test -v ./...` or `make test`
 
-# swagger
-go install github.com/swaggo/swag/cmd/swag@latest
+# For Docs we use swagger
+`go install github.com/swaggo/swag/cmd/swag@latest`
 
 ## update docs
-swag init --parseDependency
+`swag init --parseDependency`
--- config/config.go
+++ config/config.go
@@ -4,7 +4,6 @@ import "gitlab.com/avarf/getenvs"
 
 type Config struct {
 	Port         int
-	PostgresURL  string
 	ClientID     string
 	ClientSecret string
 	DBName       string
@@ -15,7 +14,6 @@ type Config struct {
 func GetConfig() Config {
 	var config Config
 
-	config.PostgresURL = getenvs.GetEnvString("DB_URL", "postgres://postgres:postgres@127.0.0.1:5432/spotifyService?sslmode=disable")
 	config.Port, _ = getenvs.GetEnvInt("PORT", 8000)
 	config.ClientID = getenvs.GetEnvString("CLIENT_ID", "99001291aa1f4dacbf5b381f1fd8af71")
 	config.ClientSecret = getenvs.GetEnvString("CLIENT_SECRET", "4c7f1a7905d14e9bbc91116909a7bea1")
